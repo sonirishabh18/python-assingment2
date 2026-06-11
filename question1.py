8764039606748dad721dc390ff583355269a6562
sonirishabh18 <sonyrishabh18@gmail.com>
2026-06-11 23:14:23 +0530

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,6 @@
+my_tuple = (10, 20, 30, 40, 50)
+
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
+print("Length of tuple:", len(my_tuple))
+print("Elements from index 1 to 3:", my_tuple[1:4])
