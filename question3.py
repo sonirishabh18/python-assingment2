5cf7b3862564ed039cea47cf7f3287f346321edf
sonirishabh18 <sonyrishabh18@gmail.com>
2026-06-11 23:15:34 +0530

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,9 @@
+numbers = {10, 20, 30, 40, 50}
+
+print("Complete set:", numbers)
+print("Length of set:", len(numbers))
+
+if 30 in numbers:
+    print("30 is present")
+else:
+    print("30 is not present")
