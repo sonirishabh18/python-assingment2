b365c2a45bc7a1e16a2290699995ecba9c680ded
sonirishabh18 <sonyrishabh18@gmail.com>
2026-06-11 23:19:48 +0530

    Create question5.py

---
+++ question5.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Kriti",
+    "age": 20,
+    "course": "Python"
+}
+
+print("Complete Dictionary:", student)
+print("Student Name:", student["name"])
+print("Student Age:", student["age"])
+print("Student Course:", student["course"])
